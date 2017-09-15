3cdfb8b768c0f30ede21758ca1f7ffc20134b9cc
belgattitude <belgattitude@gmail.com>
2017-09-15 14:06:32 +0200

    php 5.6+ support

--- tests/src/SolubleTest/DbWrapper/AdapterFactoryTest.php
+++ tests/src/SolubleTest/DbWrapper/AdapterFactoryTest.php
@@ -11,32 +11,32 @@ class AdapterFactoryTest extends TestCase
     {
         $conn = \SolubleTestFactories::getDbConnection('mysqli');
         $adapter = AdapterFactory::createAdapterFromResource($conn);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\AdapterInterface', $adapter);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\MysqliAdapter', $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\AdapterInterface::class, $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\MysqliAdapter::class, $adapter);
     }
 
     public function testCreateAdapterFromPDOMysqlConnection()
     {
         $conn = \SolubleTestFactories::getDbConnection('pdo:mysql');
         $adapter = AdapterFactory::createAdapterFromResource($conn);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\AdapterInterface', $adapter);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\PdoMysqlAdapter', $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\AdapterInterface::class, $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\PdoMysqlAdapter::class, $adapter);
     }
 
     public function testCreateAdapterFromPDOSqliteConnection()
     {
         $connection = new \PDO('sqlite::memory:');
         $adapter = AdapterFactory::createAdapterFromResource($connection);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\AdapterInterface', $adapter);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\PdoSqliteAdapter', $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\AdapterInterface::class, $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\PdoSqliteAdapter::class, $adapter);
     }
 
     public function testCreateAdapterFromZendDb2Adapter()
     {
         $zendAdapter = \SolubleTestFactories::getDbConnection('zend-db2-mysqli');
         $adapter = AdapterFactory::createAdapterFromZendDb2($zendAdapter);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\AdapterInterface', $adapter);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\Zend\ZendDb2Adapter', $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\AdapterInterface::class, $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\Zend\ZendDb2Adapter::class, $adapter);
     }
 
     public function testCreateAdapterFromDoctrine2Test()
@@ -44,8 +44,8 @@ class AdapterFactoryTest extends TestCase
         $doctrine = \SolubleTestFactories::getDbConnection('doctrine2-mysqli');
 
         $adapter = AdapterFactory::createAdapterFromDoctrine2($doctrine);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\AdapterInterface', $adapter);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\Doctrine\Dbal2Adapter', $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\AdapterInterface::class, $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\Doctrine\Dbal2Adapter::class, $adapter);
     }
 
     public function testCreateAdapterFromCapsule5Test()
@@ -53,11 +53,11 @@ class AdapterFactoryTest extends TestCase
         $capsule = \SolubleTestFactories::getDbConnection('capsule5-mysqli');
 
         $adapter = AdapterFactory::createAdapterFromCapsule5($capsule);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\AdapterInterface', $adapter);
-        self::assertInstanceOf('\Soluble\DbWrapper\Adapter\Laravel\Capsule5Adapter', $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\AdapterInterface::class, $adapter);
+        self::assertInstanceOf(\Soluble\DbWrapper\Adapter\Laravel\Capsule5Adapter::class, $adapter);
     }
 
-    public function testCreateAdapterThrowsException()
+    public function testCreateAdapterFromResourceThrowsException()
     {
         $this->expectException(\Soluble\DbWrapper\Exception\InvalidArgumentException::class);
         $fct = function () {
@@ -66,7 +66,7 @@ class AdapterFactoryTest extends TestCase
         $adapter = AdapterFactory::createAdapterFromResource($fct);
     }
 
-    public function testCreateAdapterThrowsException2()
+    public function testCreateAdapterFromResourceThrowsExceptionInvalidType()
     {
         $this->expectException(\Soluble\DbWrapper\Exception\InvalidArgumentException::class);
         $adapter = AdapterFactory::createAdapterFromResource('a');
